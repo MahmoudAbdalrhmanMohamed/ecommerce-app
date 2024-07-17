02978e9e1ca927675b7810c1dcdd5f78faecc9ce
MahmoudAbdalrhmanMohamed <mahnoudhouda5632@gmail.com>
2024-07-17 21:11:45 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Ecommerce App',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: './logo.png',
+            sizes: '50x33',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg}'],
+        sourcemap: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
