829fdc9426bc9ace4e0c05676a84b8b210792cd9
MahmoudAbdalrhmanMohamed <mahnoudhouda5632@gmail.com>
2024-07-24 02:27:43 +0300

    validate

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,18 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
-import { required, min, max, alpha_spaces as alphaSpaces, email,confirmed } from '@vee-validate/rules'
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  confirmed
+} from '@vee-validate/rules'
 export default {
   install(app) {
     app.component('VeeForm', VeeForm)
@@ -11,5 +24,24 @@ export default {
     defineRule('alphaSpaces', alphaSpaces)
     defineRule('email', email)
     defineRule('confirmed', confirmed)
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alphaSpaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          confirmed: "The passwords don't match."
+        }
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid.`
+
+        return message
+      }
+    })
   }
 }
