a1bffbc0c651334e3d3a216bca993612ac0f3a71
MahmoudAbdalrhmanMohamed <mahnoudhouda5632@gmail.com>
2024-08-06 20:56:22 +0300

    improve accessibility

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c8rp3n10ftg"
+    "revision": "0.11m38cvc84"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
